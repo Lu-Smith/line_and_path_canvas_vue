84a6c7e69369ccc666ee549bb94352495e0c19f7
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-02-19 14:57:08 +0000

    Updated Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,3 +1,13 @@
 <template>
-    Footer
+       <h3>
+        Coded by 
+        <a 
+        href="https://www.lunasmithart.com/"
+        target="_blank">
+        Luna Smith</a> -
+        <a 
+        href="https://github.com/Lu-Smith/line_and_path_canvas_vue" 
+        target="_blank">
+        GitHub</a>.
+    </h3>
 </template>
