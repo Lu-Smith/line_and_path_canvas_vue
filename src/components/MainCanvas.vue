d8202ced1aaa130ce9909de4809ae7b550c2bff1
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-02-19 18:40:34 +0000

    Updatedc MainCanvas.vue logic

--- src/components/MainCanvas.vue
+++ src/components/MainCanvas.vue
@@ -9,14 +9,18 @@
     import LineOne from '../assets/LineOne';
 
     const canvas = ref<HTMLCanvasElement | null>(null);
+    const linesArray = ref<LineOne[]>([]);
    
-   
-
     onMounted(() => {
         if (canvas.value) {
             const ctx = canvas.value.getContext('2d');
             canvas.value.width = window.innerWidth;
             canvas.value.height = window.innerHeight*0.786;
+            for (let i = 0; i < 10; i++) {
+                linesArray.value.push(new LineOne(canvas.value))
+            }
+
+            console.log(linesArray.value);
             const Line1 = new LineOne(canvas.value);
 
             if (ctx) {
