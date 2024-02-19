f0555f6f97bd1beb4032f80268355eeb016017fd
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-02-19 20:02:58 +0000

    Updated LineOne.ts

--- src/assets/LineOne.ts
+++ src/assets/LineOne.ts
@@ -8,6 +8,8 @@ export default class LineOne {
     maxLength: number;
     speedX: number;
     speedY: number;
+    lifeSpan: number;
+    timer: number;
 
     constructor(canvas: HTMLCanvasElement) {
         this.canvas = canvas;
@@ -19,6 +21,8 @@ export default class LineOne {
         this.maxLength = 10;
         this.speedX = 2;
         this.speedY = 7;
+        this.lifeSpan = this.maxLength * 10;
+        this.timer = 0;
     }
     draw(ctx: CanvasRenderingContext2D ) {
         ctx.strokeStyle = 'hsl(' + this.hue + ', 100%, 50%)';
@@ -31,11 +35,24 @@ export default class LineOne {
         ctx.stroke();
     }
     update() {
-        this.x += this.speedX + Math.random() * 50 - 25;
-        this.y += this.speedY+ Math.random() * 50 - 25;
-        this.history.push({x: this.x, y: this.y});
-        if (this.history.length > this.maxLength) {
+        this.timer++;
+        if (this.timer < this.lifeSpan) {
+            this.x += this.speedX + Math.random() * 50 - 25;
+            this.y += this.speedY+ Math.random() * 50 - 25;
+            this.history.push({x: this.x, y: this.y});
+            if (this.history.length > this.maxLength) {
+                this.history.shift();
+            }
+        } else if(this.history.length <=1) {
+            this.reset();
+        } else {
             this.history.shift();
         }
+    }
+    reset() {
+        this.x = Math.random() * this.canvas.width;
+        this.y = Math.random() * this.canvas.height;
+        this.history = [{x: this.x, y: this.y}];
+        this.timer = 0;
     }
 }
