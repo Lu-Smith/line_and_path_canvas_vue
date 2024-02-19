13ca6252ce1cefce9812c2c9c6628bcfb5aa04eb
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-02-19 17:56:05 +0000

    Updated LineOne.ts

--- src/assets/LineOne.ts
+++ src/assets/LineOne.ts
@@ -1,9 +1,19 @@
 export class LineOne {
     startX: number;
     startY: number;
+    endX: number;
+    endY: number;
 
     constructor(canvas: HTMLCanvasElement) {
         this.startX = Math.random() * canvas.width;
         this.startY = Math.random() * canvas.height;
+        this.endX = Math.random() * canvas.width;
+        this.endY = Math.random() * canvas.height;
+    }
+    draw(ctx: CanvasRenderingContext2D ) {
+        ctx.beginPath();
+        ctx.moveTo(this.startX, this.startY);
+        ctx.lineTo(this.endX, this.endY);
+        ctx.stroke();
     }
 }
