82f8d9f4f653b341a75dfa0b184fbcff08692cd6
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-02-19 19:36:24 +0000

    Updated LineOne.ts

--- src/assets/LineOne.ts
+++ src/assets/LineOne.ts
@@ -5,6 +5,7 @@ export default class LineOne {
     lineWidth: number
     canvas: HTMLCanvasElement;
     hue: number;
+    maxLength: number;
 
     constructor(canvas: HTMLCanvasElement) {
         this.canvas = canvas;
@@ -13,6 +14,7 @@ export default class LineOne {
         this.history = [{x: this.x, y: this.y}];
         this.lineWidth = Math.floor(Math.random() * 15 + 1);
         this.hue = Math.floor(Math.random() * 360);
+        this.maxLength = 10;
     }
     draw(ctx: CanvasRenderingContext2D ) {
         ctx.strokeStyle = 'hsl(' + this.hue + ', 100%, 50%)';
@@ -28,5 +30,8 @@ export default class LineOne {
         this.x = Math.random() * this.canvas.width;
         this.y = Math.random() * this.canvas.height;
         this.history.push({x: this.x, y: this.y});
+        if (this.history.length > this.maxLength) {
+            this.history.shift();
+        }
     }
 }
