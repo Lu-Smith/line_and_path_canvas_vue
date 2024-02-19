c7bf3be93ba86b5d3096a54230730607bd78417d
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-02-19 17:35:24 +0000

    Updated MainCanvas.vue

--- src/components/MainCanvas.vue
+++ src/components/MainCanvas.vue
@@ -9,29 +9,18 @@
 
     const canvas = ref<HTMLCanvasElement | null>(null);
 
- 
-    
-    const handleMouseDown = () => {}
-
     onMounted(() => {
-        window.addEventListener("mousedown", handleMouseDown);
         if (canvas.value) {
-        const ctx = canvas.value.getContext('2d');
-        canvas.value.width = window.innerWidth;
-        canvas.value.height = window.innerHeight*0.786;
-        console.log(ctx);
+            const ctx = canvas.value.getContext('2d');
+            canvas.value.width = window.innerWidth;
+            canvas.value.height = window.innerHeight*0.786;
+            console.log(ctx);
 
-        if (ctx) {
-            ctx.lineWidth = 20;
-  
-            ctx.beginPath();
-            ctx.moveTo(200, 100);
-            ctx.lineTo(150, 400);
-            ctx.stroke();
+            if (ctx) {
+                ctx.lineWidth = 20;
+                ctx.strokeStyle = 'magenta';
+            }
         }
-
-
-    }
     });
 </script>
 
