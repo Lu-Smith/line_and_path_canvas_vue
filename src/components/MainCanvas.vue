6789a7bc2bf5adea8b9a958a8dacaf6731ba4c58
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-02-19 16:18:13 +0000

    Updated MainCanvas.vue

--- src/components/MainCanvas.vue
+++ src/components/MainCanvas.vue
@@ -1,7 +1,17 @@
 <template>
-    canvas
+    <canvas ref="canvas"></canvas>
 </template>
 
 <script lang="ts" setup>
     defineProps(['mode']);
+
+    import { ref, onMounted } from 'vue';
+
+    const canvas = ref<HTMLCanvasElement | null>(null);
+
+    const handleMouseDown = () => {}
+
+    onMounted(() => {
+        window.addEventListener("mousedown", handleMouseDown);
+    });
 </script>
