c4b16fd0dc8a4b56235705f5d5ab23717e9f6395
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-02-19 18:11:18 +0000

    Updated LineOne.ts

--- src/assets/LineOne.ts
+++ src/assets/LineOne.ts
@@ -3,14 +3,17 @@ export default class LineOne {
     startY: number;
     endX: number;
     endY: number;
+    lineWidth: number;
 
     constructor(canvas: HTMLCanvasElement) {
         this.startX = Math.random() * canvas.width;
         this.startY = Math.random() * canvas.height;
         this.endX = Math.random() * canvas.width;
         this.endY = Math.random() * canvas.height;
+        this.lineWidth = Math.floor(Math.random() * 15 + 1);
     }
     draw(ctx: CanvasRenderingContext2D ) {
+        ctx.lineWidth = this.lineWidth;
         ctx.beginPath();
         ctx.moveTo(this.startX, this.startY);
         ctx.lineTo(this.endX, this.endY);
