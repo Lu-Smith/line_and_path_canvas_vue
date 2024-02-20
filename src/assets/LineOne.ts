b30d01aad8281f918a793fa943ff78bcc395f044
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-02-20 14:34:38 +0000

    Updated LineOne.ts

--- src/assets/LineOne.ts
+++ src/assets/LineOne.ts
@@ -19,9 +19,9 @@ export default class LineOne {
         this.lineWidth = Math.floor(Math.random() * 15 + 1);
         this.hue = Math.floor(Math.random() * 360);
         this.maxLength = Math.floor(Math.random() * 150 + 10);
-        this.speedX = Math.random() * 5 - 2.5;
+        this.speedX = Math.random() * 1 - 0.5;
         this.speedY = 7;
-        this.lifeSpan = this.maxLength * 10;
+        this.lifeSpan = this.maxLength * 3;
         this.timer = 0;
     }
     draw(ctx: CanvasRenderingContext2D ) {
@@ -37,8 +37,8 @@ export default class LineOne {
     update() {
         this.timer++;
         if (this.timer < this.lifeSpan) {
-            this.x += this.speedX + Math.random() * 50 - 25;
-            this.y += this.speedY+ Math.random() * 50 - 25;
+            this.x += this.speedX + Math.random() * 30 - 15;
+            this.y += this.speedY+ Math.random() * 20 - 25;
             this.history.push({x: this.x, y: this.y});
             if (this.history.length > this.maxLength) {
                 this.history.shift();
