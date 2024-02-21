5b0f710d04edf01c6bbe8416ec1790fff5bd7e86
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-02-21 18:24:35 +0000

    Updated mode styles in Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,11 +3,13 @@
         Coded by 
         <a 
         href="https://www.lunasmithart.com/"
-        target="_blank">
+        target="_blank"
+        :style="{ color: mode ? '#feffdf' : '#f3bc77' }">
         Luna Smith</a> -
         <a 
         href="https://github.com/Lu-Smith/line_and_path_canvas_vue" 
-        target="_blank">
+        target="_blank"
+        :style="{ color: mode ? '#feffdf' : '#f3bc77' }">
         GitHub</a>.
     </h3>
 </template>
